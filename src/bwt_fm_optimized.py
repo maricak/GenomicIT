a5bf2f16ca6f4131d96ffc97a0f74823c77b7894
Marija Kostic <marijakostic25@gmail.com>
2020-03-30 12:09:26 +0200

    Refactor tests and fomat code

--- src/bwt_fm_optimized.py
+++ src/bwt_fm_optimized.py
@@ -1,9 +1,15 @@
 import bwt_fm_interface as bfi
 
-class BwtFmOptimized(bfi.BwtFmInterface):
-    def __init__(self, text):
-        super().__init__(text)
 
-    def _suffix_array(self):
-        suffix_matrix = sorted([(self._text[i:], i) for i in range(len(self._text))])
+class BwtFmOptimized(bfi.BwtFmInterface):
+    def __init__(self, text, suffix_array_factor):
+        super().__init__(text)
+        self._suffix_array = self._downsample_suffix_array(suffix_array_factor)
+
+    def _build_suffix_array(self):
+        suffix_matrix = sorted([(self._text[i:], i)
+                                for i in range(len(self._text))])
         return list(map(lambda suffix_index: suffix_index[1], suffix_matrix))
+
+    def _downsample_suffix_array(self, factor):
+        return [position for index, position in enumerate(self._suffix_array) if index % factor == 0]
--- src/bwt_fm_simple.py
+++ src/bwt_fm_simple.py
@@ -1,9 +1,11 @@
 import bwt_fm_interface as bfi
 
+
 class BwtFmSimple(bfi.BwtFmInterface):
     def __init__(self, text):
         super().__init__(text)
 
-    def _suffix_array(self):
-        suffix_matrix = sorted([(self._text[i:], i) for i in range(len(self._text))])
+    def _build_suffix_array(self):
+        suffix_matrix = sorted([(self._text[i:], i)
+                                for i in range(len(self._text))])
         return list(map(lambda suffix_index: suffix_index[1], suffix_matrix))
